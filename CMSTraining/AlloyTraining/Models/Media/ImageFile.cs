d946324ecdaaed95e6fcc07c87ee295e93eeb74f
Cương Nguyễn <cuong0993@gmail.com>
2021-06-21 18:20:38 +0700

    Managing media assets

---
+++ CMSTraining/AlloyTraining/Models/Media/ImageFile.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlloyTraining.Models.Media
+{
+    [ContentType(DisplayName = "ImageFile", GUID = "ff0396c9-1c36-4e46-a298-fe715cd93b41", Description = "Use this to upload image files.”")]
+    [MediaDescriptor(ExtensionString = "png,gif,jpg,jpeg")]
+    public class ImageFile : ImageData
+    {
+        
+                [CultureSpecific]
+                [Editable(true)]
+                public virtual string Description { get; set; }
+         
+    }
+}
