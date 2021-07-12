88c8d526695392871f1c64835c667a16254de415
Cương Nguyễn <cuong0993@gmail.com>
2021-07-12 16:52:16 +0700

    Content Approvals Manager

---
+++ CMSTraining/AlloyDemo/Features/ContentApprovals/ContentApprovalsManagerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using AlloyDemo.Models.ViewModels;
+using EPiServer.Approvals.ContentApprovals;
+
+namespace AlloyDemo.Features.ContentApprovals
+{
+    public class ContentApprovalsManagerViewModel : PageViewModel<ContentApprovalsManagerPage>
+    {
+        public ContentApprovalDefinition ApprovalDefinition { get; set; }
+        public ContentApproval Approval { get; set; }
+
+        public ContentApprovalsManagerViewModel(
+            ContentApprovalsManagerPage currentPage) : base(currentPage)
+        {
+        }
+    }
+}
