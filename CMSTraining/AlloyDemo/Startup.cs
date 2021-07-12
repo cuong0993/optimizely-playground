de40ad8757ff28d782351bee372b469441327d94
Cương Nguyễn <cuong0993@gmail.com>
2021-07-12 16:01:26 +0700

    Reset password

--- CMSTraining/AlloyDemo/Startup.cs
+++ CMSTraining/AlloyDemo/Startup.cs
@@ -1,3 +1,4 @@
+using AlloyAdvanced.Features.ResetAdmin;
 using AlloyDemo.Features.RegisterPersonas;
 using EPiServer.Cms.UI.AspNetIdentity;
 using Microsoft.AspNet.Identity;
@@ -45,6 +46,7 @@ namespace AlloyDemo
                 }
             });
             app.UseRegisterPersonas(() => HttpContext.Current.Request.IsLocal);
+            app.UseResetAdmin(() => HttpContext.Current.Request.IsLocal);
         }
     }
 }
