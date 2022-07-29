5ccbf2fe396e2fbf007658e814693698f54e946c
Cương Nguyễn <cuong0993@gmail.com>
2022-07-29 10:14:40 +0700

    Error controller

---
+++ MSWebPlayground/MSWebPlayground/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MSWebPlayground.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public string Index()
+        {
+            return "This is an error";
+        }
+    }
+}
--- MSWebPlayground/MSWebPlayground/Global.asax.cs
+++ MSWebPlayground/MSWebPlayground/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MSWebPlayground.Controllers;
 
 namespace MSWebPlayground
 {
@@ -13,6 +15,25 @@ namespace MSWebPlayground
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+            var httpContext = ((HttpApplication) sender).Context;
+            httpContext.Response.Clear();
+            httpContext.ClearError();
+            httpContext.Response.TrySkipIisCustomErrors = true;
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "error";
+            routeData.Values["action"] = "index";
+            routeData.Values["exception"] = exception;
+
+            using (var controller = new ErrorController())
+            {
+                ((IController) controller).Execute(
+                    new RequestContext(new HttpContextWrapper(httpContext), routeData));
+            }
         }
     }
 }
