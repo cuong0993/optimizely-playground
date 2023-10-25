96b9e67afb31977ac21752ffd8a1111c1d819415
cuong0993 <cuong0993@gmail.com>
2023-10-25 15:44:23 +0700

    Add Azure AD

--- Alloy/Startup.cs
+++ Alloy/Startup.cs
@@ -1,13 +1,18 @@
+using System.Security.Claims;
+using System.Text;
 using Alloy.Extensions;
 using EPiServer.Cms.Shell;
 using EPiServer.Cms.TinyMce;
 using EPiServer.Cms.UI.Admin;
-using EPiServer.Cms.UI.AspNetIdentity;
 using EPiServer.Cms.UI.VisitorGroups;
 using EPiServer.Events.Providers.Internal;
 using EPiServer.Scheduler;
+using EPiServer.Security;
 using EPiServer.Web.Mvc.Html;
 using EPiServer.Web.Routing;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Alloy;
 
@@ -36,10 +41,8 @@ public class Startup
         }
 
         services
-            .AddCmsAspNetIdentity<ApplicationUser>()
             .AddCmsHost().AddCmsHtmlHelpers().AddCmsTagHelpers().AddCmsUI().AddAdmin().AddVisitorGroupsUI().AddTinyMce()
             .AddAlloy()
-            .AddAdminUserRegistration()
             .AddEmbeddedLocalization<Startup>();
 
         // Required by Wangkanai.Detection
@@ -51,6 +54,72 @@ public class Startup
             options.Cookie.HttpOnly = true;
             options.Cookie.IsEssential = true;
         });
+
+        IdentityModelEventSource.ShowPII = true;
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "azure-cookie";
+                options.DefaultChallengeScheme = "azure";
+            })
+            .AddCookie("azure-cookie", options =>
+            {
+                options.Events.OnSignedIn = async ctx =>
+                {
+                    if (ctx.Principal?.Identity is ClaimsIdentity claimsIdentity)
+                    {
+                        // Syncs user and roles so they are available to the CMS
+                        var synchronizingUserService = ctx
+                            .HttpContext
+                            .RequestServices
+                            .GetRequiredService<ISynchronizingUserService>();
+
+                        await synchronizingUserService.SynchronizeAsync(claimsIdentity);
+                    }
+                };
+            })
+            .AddOpenIdConnect("azure", options =>
+            {
+                options.SignInScheme = "azure-cookie";
+                options.SignOutScheme = "azure-cookie";
+                options.ResponseType = OpenIdConnectResponseType.Code;
+                options.CallbackPath = "/signin-oidc";
+                options.UsePkce = true;
+
+                // If Azure AD is register for multi-tenant
+                //options.Authority = "https://login.microsoftonline.com/" + "common" + "/v2.0";
+                options.Authority = _configuration["Authentication:Authority"];
+                options.ClientId = _configuration["Authentication:ClientId"];
+                options.ClientSecret = _configuration["Authentication:ClientSecret"];
+
+                options.Scope.Clear();
+                options.Scope.Add(OpenIdConnectScope.OpenIdProfile);
+                options.Scope.Add(OpenIdConnectScope.OfflineAccess);
+                options.Scope.Add(OpenIdConnectScope.Email);
+                options.MapInboundClaims = false;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    RoleClaimType = "roles",
+                    NameClaimType = "preferred_username",
+                    ValidateIssuer = false
+                };
+
+                options.Events.OnRedirectToIdentityProvider = ctx =>
+                {
+                    // Prevent redirect loop
+                    if (ctx.Response.StatusCode == 401) ctx.HandleResponse();
+
+                    return Task.CompletedTask;
+                };
+
+                options.Events.OnAuthenticationFailed = context =>
+                {
+                    context.HandleResponse();
+                    context.Response.BodyWriter.WriteAsync(Encoding.ASCII.GetBytes(context.Exception.Message));
+                    return Task.CompletedTask;
+                };
+            });
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
