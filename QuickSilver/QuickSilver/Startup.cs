4910cc553221c34d78141f0fb377930cf6e343be
cuong0993 <cuong0993@gmail.com>
2023-11-03 15:38:03 +0700

    Add service api
    (cherry picked from commit dcab79e1a2b029164aa1e7fc721c571fd68aadae)
    (cherry picked from commit 518418d5b157df9fa66712ef01b76480896a2717)

--- QuickSilver/QuickSilver/Startup.cs
+++ QuickSilver/QuickSilver/Startup.cs
@@ -1,6 +1,8 @@
 using EPiServer.Cms.Shell;
 using EPiServer.Cms.UI.AspNetIdentity;
+using EPiServer.OpenIDConnect;
 using EPiServer.Scheduler;
+using EPiServer.ServiceApi;
 using EPiServer.ServiceLocation;
 using EPiServer.Web.Routing;
 using Mediachase.Commerce.Anonymous;
@@ -30,6 +32,41 @@ namespace QuickSilver
                 .AddCommerce()
                 .AddAdminUserRegistration()
                 .AddEmbeddedLocalization<Startup>();
+            services.AddOpenIDConnect<SiteUser>(
+                useDevelopmentCertificate: true,
+                signingCertificate: null,
+                encryptionCertificate: null,
+                createSchema: true,
+                options =>
+                {
+                    //options.RequireHttps = !_webHostingEnvironment.IsDevelopment();
+                    var application = new OpenIDConnectApplication()
+                    {
+                        ClientId = "postman-client",
+                        ClientSecret = "postman",
+                        Scopes =
+                        {
+                            ServiceApiOptionsDefaults.Scope
+                        }
+                    };
+
+                    // Using Postman for testing purpose.
+                    // The authorization code is sent to postman after successful authentication.
+                    application.RedirectUris.Add(new Uri("https://oauth.pstmn.io/v1/callback"));
+                    options.Applications.Add(application);
+                    options.AllowResourceOwnerPasswordFlow = true;
+
+                    options.Applications.Add(new OpenIDConnectApplication()
+                    {
+                        ClientId = "anon-client",
+                        Scopes = {
+                            "anonymous_id"
+                        }
+                    });
+                    options.AllowAnonymousFlow = true;
+                });
+
+            services.AddOpenIDConnectUI();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
